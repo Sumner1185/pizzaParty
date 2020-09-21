63042c3d3c39ec2a1925eb3377140eb87a5577b5
Sumner1185 <alastair.sumner@yahoo.com>
2020-09-21 20:53:36 +0100

    Refactor functions

--- pizzaParty.js
+++ pizzaParty.js
@@ -1,14 +1,14 @@
 const SLICES_PER_PIZZA = 8
 
-function numberOfSlices(people, pizza) {
-  return Math.floor((pizza * SLICES_PER_PIZZA) / people)
- }
+function getInfo() {
+  let people = window.prompt('How many people?')
+  let pizza = window.prompt('How many pizzas do you have?')
+  let result = displayResults(people, pizza);
 
- function remainingSlices(people, pizza) {
-  return (pizza * SLICES_PER_PIZZA) % people
- }
+  return result
+}
 
- function displayResults(people, pizza) {
+function displayResults(people, pizza) {
   let slices = numberOfSlices(people, pizza)
   let leftOvers = remainingSlices(people, pizza)
   let firstLine = `${people} people with ${pizza} pizzas.`
@@ -16,13 +16,12 @@ function numberOfSlices(people, pizza) {
   let thirdLine = `There are ${leftOvers} leftover pieces.`
 
   return `${firstLine}\n${secondLine}\n${thirdLine}`
- }
+}
 
- function getInfo() {
-    let people = window.prompt('How many people?')
-    let pizza = window.prompt('How many pizzas do you have?')
-    let result = displayResults(people, pizza);
+function numberOfSlices(people, pizza) {
+  return Math.floor((pizza * SLICES_PER_PIZZA) / people)
+}
 
-    return result
-    // document.write (result);
- }
+function remainingSlices(people, pizza) {
+  return (pizza * SLICES_PER_PIZZA) % people
+}
